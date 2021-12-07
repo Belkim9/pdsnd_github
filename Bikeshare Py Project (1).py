631b049088be8398132eddf0dd90413701df0504
Belkim9 <95422674+Belkim9@users.noreply.github.com>
2021-12-07 20:53:53 +0100

    Update Bikeshare Py Project (1).py

--- Bikeshare Py Project (1).py
+++ Bikeshare Py Project (1).py
@@ -23,6 +23,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print("Hello! Let\'s explore some US bikeshare data!")
+	print("\nThere is data available for three different cities.")
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = ""
     while city not in CITY_DATA.keys():
@@ -244,7 +245,7 @@ def main():
         display_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n').lower()
-        print()
+        print("Goodbye")
         if restart != 'yes' and restart != 'y' and restart != 'yus':
             break
 
